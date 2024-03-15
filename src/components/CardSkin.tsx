9222f5d3bfece1fe133dc5097866d460c84e1418
Marcos <marcosraach.1@gmail.com>
2024-03-14 22:38:56 -0300

    feat: Skin card with Input

--- src/components/CardSkin.tsx
+++ src/components/CardSkin.tsx
@@ -1,34 +1,76 @@
 'use client'
-import { getAllSkins } from "@/services/services";
-import { useEffect, useState } from "react";
+import { getAllSkins } from "@/services/services"
+import { useEffect, useState } from "react"
+import { Input } from "./Input"
 
 type Skin = {
   name: string
   image: string
   price: number
   weapon: string
-  pattern: string
-  souvenir: string
 }
 
 export function CardSkin() {
-  const [skins, setSkins] = useState<Skin[]>([]);
+  const [skins, setSkins] = useState<Skin[]>([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [zoomLevel, setZoomLevel] = useState(2)
 
   useEffect(() => {
-    getAllSkins().then((response) => {
-      setSkins(response.data);
-    });
+    getAllSkins()
+      .then((response) => {
+        setSkins(response.data)
+      });
   }, []);
 
+  const indexOfFirstSkin = currentPage - 1
+  const currentSkins = skins.slice(indexOfFirstSkin, currentPage)
+
+  function nextPage() {
+    if (currentPage < Math.ceil(skins.length)) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  function prevPage() {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
+  function handleGuessSubmit(guessSkin: string) {
+    const currentIndex = currentPage - 1
+    const currentSkin = currentSkins[currentIndex]
+    if (currentSkin.name === guessSkin) {
+      console.log('Você acertou!')
+    } else {
+      console.log('Você errou!')
+    }
+    setZoomLevel(zoomLevel - 0.1)
+  }
+
   return (
-    <div>
-      {skins.map((skin) => (
+    <div className="flex justify-center items-center min-h-screen bg-gray-700 text-white">
+      <div className="bg-gray-900 p-6 rounded-lg shadow-lg">
+        {currentSkins.map((skin, index) => (
+          <div key={index} className="mb-6" style={{ width: '100%', overflow: 'hidden' }}>
+            <div style={{ transform: `scale(${zoomLevel})`, transition: 'transform 0.5s ease', transformOrigin: 'top left' }}>
+              <img src={skin.image} alt="A imagem não carregou." className="rounded-lg shadow-md" style={{ transform: `scale(${zoomLevel})`, transition: 'transform 0.5s ease' }} />
+            </div>
+            <p className="mt-2">Name: {skin.name}</p>
+          </div>
+        ))}
         <div>
-            <h1>
-              {skin.name}
-            </h1>
+          <Input onSubmit={handleGuessSubmit} />
+          <div className="flex justify-between mt-4">
+            <button onClick={prevPage} disabled={currentPage === 1} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              Previous
+            </button>
+            <button onClick={nextPage} disabled={currentPage === Math.ceil(skins.length)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              Next
+            </button>
+          </div>
         </div>
-      ))}
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+type InputProps = {
+  onSubmit: (inputValue: string) => void;
+};
+
+export function Input({ onSubmit }: InputProps) {
+  const [inputValue, setInputValue] = useState('');
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  }
+
+  function handleSubmit() {
+    onSubmit(inputValue);
+    setInputValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Chute qual skin você acha que é: "
+        value={inputValue}
+        onChange={handleInputChange}
+        className="border border-gray-400 rounded px-2 py-1 text-black focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+        style={{ width: '80%' }}
+      />
+      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded ml-2">
+        Chutar
+      </button>
+    </form>
+  );
+};
