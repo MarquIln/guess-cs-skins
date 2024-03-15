40010578f94135288e6a7ba3d2fc5b186b1db362
Marcos <marcosraach.1@gmail.com>
2024-03-15 13:44:53 -0300

    feat: update skin name on nextPage

--- src/components/CardSkin.tsx
+++ src/components/CardSkin.tsx
@@ -1,66 +1,82 @@
 'use client'
-import { getAllSkins } from "@/services/services";
-import { Input } from "./Input";
-import { useEffect, useState } from "react";
+import { getAllSkins } from "@/services/services"
+import { useEffect, useState } from "react"
+import { Input } from "./Input"
 
 type Skin = {
-  name: string;
-  image: string;
-  price: number;
-  weapon: string;
-};
+  name: string
+  image: string
+  price: number
+  weapon: string
+}
 
 export function CardSkin() {
-  const [skins, setSkins] = useState<Skin[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [zoomLevel, setZoomLevel] = useState(5);
-  const [guessCorrect, setGuessCorrect] = useState(false);
-  const [currentSkinName, setCurrentSkinName] = useState("");
+  const [skins, setSkins] = useState<Skin[]>([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [zoomLevel, setZoomLevel] = useState(5)
+  const [guessCorrect, setGuessCorrect] = useState(false)
+  const [currentSkinName, setCurrentSkinName] = useState("")
+  const [minZoomLevel] = useState(1)
+
+  const indexOfFirstSkin = currentPage - 1
+  const currentSkins = skins.slice(indexOfFirstSkin, currentPage)
 
   useEffect(() => {
     getAllSkins().then((response) => {
-      setSkins(response.data);
-    });
-  }, []);
+      const shuffledSkins = shuffleArray(response.data)
+      setSkins(shuffledSkins)
+    })
+  }, [])
 
   useEffect(() => {
-    const indexOfFirstSkin = currentPage - 1;
-    const currentIndex = indexOfFirstSkin >= 0 ? indexOfFirstSkin : 0;
-    setCurrentSkinName(skins[currentIndex]?.name || "");
-    console.log(skins[currentIndex]?.name)
-  }, [currentPage, skins]);
+    const currentSkin = skins[currentPage - 1]
+    setCurrentSkinName(currentSkin?.name || "")
+  }, [currentPage, skins])
 
   useEffect(() => {
     if (guessCorrect) {
-      nextPage();
-      setGuessCorrect(false);
+      nextPage()
+      setGuessCorrect(false)
     }
-  }, [guessCorrect]);
+  }, [guessCorrect])
 
-  const indexOfFirstSkin = currentPage - 1;
-  const currentSkins = skins.slice(indexOfFirstSkin, currentPage);
+  function shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+    return array
+  }
 
   function nextPage() {
     if (currentPage < Math.ceil(skins.length)) {
-      setCurrentPage(currentPage + 1);
+      setCurrentPage(currentPage + 1)
     }
   }
 
   function prevPage() {
     if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
+      setCurrentPage(currentPage - 1)
     }
   }
 
   function handleGuessSubmit(guessSkin: string) {
-    const currentIndex = currentPage - 1;
-    const currentSkin = currentSkins[currentIndex];
-    if (currentSkin?.name === guessSkin) {
-      setGuessCorrect(true);
-      console.log("Você acertou!");
-    } else {
-      setZoomLevel(zoomLevel - 0.5);
-      console.log("Você errou!");
+    const currentSkin = skins[currentPage - 1]
+    const skinNameParts = currentSkin?.name.split('|')
+
+    if (skinNameParts && skinNameParts.length === 2) {
+      const correctGuess = skinNameParts[1].trim().toLocaleLowerCase()
+      guessSkin = guessSkin.trim().toLocaleLowerCase()
+
+      if (correctGuess === guessSkin) {
+        setGuessCorrect(true)
+        console.log("Você acertou!")
+      } else {
+        console.log(correctGuess, guessSkin)
+        const newZoomLevel = zoomLevel - 0.2
+        setZoomLevel(newZoomLevel < minZoomLevel ? minZoomLevel : newZoomLevel)
+        console.log("Você errou!")
+      }
     }
   }
 
@@ -69,7 +85,7 @@ export function CardSkin() {
       <div className="bg-gray-900 p-6 rounded-lg shadow-lg">
         {currentSkins.map((skin, index) => (
           <div key={index} className="mb-6" style={{ width: "100%", overflow: "hidden" }}>
-            <div style={{ transform: `scale(${zoomLevel})`, transition: "transform 0.5s ease", transformOrigin: "top left" }}>
+            <div style={{ transform: `scale(${zoomLevel})`, transition: "transform 0.5s ease" }}>
               <img src={skin.image} alt="A imagem não carregou." className="rounded-lg shadow-md" style={{ transform: `scale(${zoomLevel})`, transition: "transform 0.5s ease" }} />
             </div>
             <p className="mt-2">Name: {currentSkinName}</p>
@@ -88,5 +104,5 @@ export function CardSkin() {
         </div>
       </div>
     </div>
-  );
+  )
 }
