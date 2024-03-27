3c3f85d2b0eb8c67b7ed559ecd94ae213b6d0f3e
Marcos <marcosraach.1@gmail.com>
2024-03-27 18:06:41 -0300

    fix: response list itens

--- src/components/ResponseList.tsx
+++ src/components/ResponseList.tsx
@@ -8,9 +8,9 @@ interface ResponseListProps {
 
 export function ResponseList({ responses }: ResponseListProps) {
   return (
-    <div>
+    <div className="flex flex-wrap justify-center">
       {responses.map((response, index) => (
-        <div key={index}>
+        <div key={index} className="flex">
           <Card
             key={index}
             borderRadius="10px"
@@ -19,6 +19,36 @@ export function ResponseList({ responses }: ResponseListProps) {
           >
             <CardBody>
               <Text>{response.name}</Text>
+            </CardBody>
+          </Card>
+          <Card
+            key={index}
+            borderRadius="10px"
+            className="hover:-translate-y m-2 w-full transform gap-2 rounded-md border border-red-800 bg-red-800 p-2 text-center text-white shadow-md transition duration-300 ease-in-out hover:border-red-700 hover:bg-red-700 hover:shadow-lg"
+            color={'white'}
+          >
+            <CardBody>
+              <Text>{response.weapon.name}</Text>
+            </CardBody>
+          </Card>
+          <Card
+            key={index}
+            borderRadius="10px"
+            className="hover:-translate-y m-2 w-full transform gap-2 rounded-md border border-red-800 bg-red-800 p-2 text-center text-white shadow-md transition duration-300 ease-in-out hover:border-red-700 hover:bg-red-700 hover:shadow-lg"
+            color={'white'}
+          >
+            <CardBody>
+              <Text>{response.rarity.name}</Text>
+            </CardBody>
+          </Card>
+          <Card
+            key={index}
+            borderRadius="10px"
+            className="hover:-translate-y m-2 w-full transform gap-2 rounded-md border border-red-800 bg-red-800 p-2 text-center text-white shadow-md transition duration-300 ease-in-out hover:border-red-700 hover:bg-red-700 hover:shadow-lg"
+            color={'white'}
+          >
+            <CardBody>
+              <Text>{response.category.name}</Text>
             </CardBody>
           </Card>
         </div>
